cf902422d998297fc3f157c3728d33cf4a6d0160
Grant Schroeder <gschroeder@pznma.com>
2019-08-08 02:45:36 +0000

    Adding Database

--- spec/helpers/grams_helper_spec.rb
+++ spec/helpers/grams_helper_spec.rb
@@ -11,10 +11,6 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe GramsController, type: :controller do
-  describe "grams#index action" do
-    it "should successfully show the page" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+
+  
 end
