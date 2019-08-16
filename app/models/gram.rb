7ab94da3cbcea0874bb8eb4d9b81ab634c860156
Grant Schroeder <gschroeder@pznma.com>
2019-08-16 04:25:03 +0000

    Update 1.0

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -2,6 +2,7 @@ class Gram < ApplicationRecord
     validates :message, presence: true
     validates :picture, presence: true
     has_many :comments
-    belongs_to :user
+
     mount_uploader :picture, PictureUploader
-end
+    belongs_to :user
+    end
