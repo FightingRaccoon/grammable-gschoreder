cf7ac4612ad5fdf9e9a8bfb675561985b55def39
Grant Schroeder <gschroeder@pznma.com>
2019-08-18 04:57:21 +0000

    Trying to figure out heroku error

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -16,11 +16,11 @@ class GramsController < ApplicationController
 
   def create
     @gram = current_user.grams.create(gram_params)
-    if @gram.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
+      if @gram.valid?
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
+      end
   end
 
   def edit
